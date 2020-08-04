2be15da8fc8dabae61a0cf715dccc2527e74203d
Daoyuan Zhai <dzhai@C02ZN21GLVDQ.myfamily.int>
2020-08-04 17:27:24 -0600

    adding density and alphashape computation

--- map-handler.py
+++ map-handler.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 xlOrders = pd.ExcelFile(r'./resources/output/orderIn60Miles.xlsx')
 xlPolys = pd.ExcelFile(r'./resources/output/polygons.xlsx')
-dfPercents = pd.read_excel(r'./resources/output/polyIn60MilesPercent.xlsx')
+xlPercents = pd.ExcelFile(r'./resources/output/polyIn60MilesPercent.xlsx')
 
 # import street map
 street_map = gpd.read_file('./resources/tl_2018_us_necta/tl_2018_us_necta.shp')
@@ -13,22 +13,34 @@ street_map = gpd.read_file('./resources/tl_2018_us_necta/tl_2018_us_necta.shp')
 crs = {'init': 'EPSG:4326'}
 
 index = 0
+dfPercents = xlPercents.parse("Percentage")
 for city in dfPercents["City"]:
+    print(city)
     if city == "Boston" or city == "US Total":
         continue
     dfOrders = xlOrders.parse(city)
     dfPolys = xlPolys.parse(city)
 
+    dfOrdersInside = xlPercents.parse(city + " Inside Orders")
+    dfOrdersBoundary = xlPercents.parse(city + " Boundary Orders")
+
     # zip x and y coordinates into single feature
     geometryOrders = [Point(x, y) for x, y in zip(dfOrders['Lng'], dfOrders['Lat'])]
-    geometryPolys = [Point(x, y) for x, y in zip(dfPolys['Lng'], dfPolys['Lat'])]
+    # geometryPolys = [Point(x, y) for x, y in zip(dfPolys['Lng'], dfPolys['Lat'])]
+    geometryPolys = [Point(x, y) for x, y in zip(dfOrdersBoundary['Lng'], dfOrdersBoundary['Lat'])]
 
     # create GeoPandas dataframe
     geoDfOrders = gpd.GeoDataFrame(dfOrders, crs=crs, geometry=geometryOrders)
 
-    geoDfPolys = gpd.GeoDataFrame(dfPolys, crs=crs, geometry=geometryPolys)
+    geoDfPolys = gpd.GeoDataFrame(dfOrdersBoundary, crs=crs, geometry=geometryPolys)
     polygon = Polygon(geoDfPolys['geometry'].tolist())
     geoDfPolys = gpd.GeoDataFrame(geometry=[polygon], crs=crs)
+
+    geoDfPolysArea = geoDfPolys.copy()
+    geoDfPolysArea = geoDfPolysArea.to_crs({'init': 'epsg:32633'})
+    geoDfPolysArea["area"] = geoDfPolysArea['geometry'].area / 10 ** 6  # km2
+    orderDensity = len(dfOrdersInside) / geoDfPolysArea["area"][0]
+    # print(orderDensity)
 
     # create figure and axes, assign to subplot
     fig, ax = plt.subplots(figsize=(15, 15))
@@ -39,9 +51,13 @@ for city in dfPercents["City"]:
     # add geodataframe to axes
     geoDfPolys.boundary.plot(color=None, edgecolor='k', ax=ax)
     geoDfOrders.plot(column='Distance', ax=ax, alpha=0.5, legend=True, markersize=4)
+    # geoDfOrders.plot(ax=ax, alpha=0.5, legend=True, markersize=4)
 
     # add title to graph
-    plt.title(city + " (" + dfPercents["Percentage"][index] + ")", fontsize=15, fontweight='bold')
+    plt.title(city + " (" + dfPercents["Percentage"][index] + " inside, " +
+              "{:.2f}".format(orderDensity) + " orders/km2, Total Area: " +
+              "{:.2f}".format(geoDfPolysArea["area"][0]) + "km2)",
+              fontsize=15, fontweight='bold')
 
     # set latitiude and longitude boundaries for map display
     xMin = min(geoDfOrders['Lng'])
@@ -56,3 +72,4 @@ for city in dfPercents["City"]:
     # plt.show()
     plt.savefig("./resources/output/figures/" + city + ".png")
     index += 1
+
--- polygon-percent.py
+++ polygon-percent.py
@@ -1,11 +1,50 @@
 import pandas as pd
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
+import alphashape
 
-cities = ["Pittsburgh", "Chicago", "San Francisco", "Detroit", "Houston", "Durham", "Indianapolis", "Birmingham", "Orlando",
+
+def find_edges_with(i, edge_set):
+    i_first = [j for (x, j) in edge_set if x == i]
+    i_second = [j for (j, x) in edge_set if x == i]
+    return i_first, i_second
+
+
+def stitch_boundaries(edges):
+    edge_set = edges.copy()
+    boundary_lst = []
+    while len(edge_set) > 0:
+        boundary = []
+        edge0 = edge_set.pop()
+        boundary.append(edge0)
+        last_edge = edge0
+        while len(edge_set) > 0:
+            i, j = last_edge
+            j_first, j_second = find_edges_with(j, edge_set)
+            if j_first:
+                edge_set.remove((j, j_first[0]))
+                edge_with_j = (j, j_first[0])
+                boundary.append(edge_with_j)
+                last_edge = edge_with_j
+            elif j_second:
+                edge_set.remove((j_second[0], j))
+                edge_with_j = (j, j_second[0])  # flip edge rep
+                boundary.append(edge_with_j)
+                last_edge = edge_with_j
+
+            if edge0[0] == last_edge[1]:
+                break
+
+        boundary_lst.append(boundary)
+    return boundary_lst
+
+
+cities = ["Pittsburgh", "Chicago", "San Francisco", "Detroit", "Houston", "Durham", "Indianapolis", "Birmingham",
+          "Orlando",
           "Nashville", "Tampa", "Seattle", "San Antonio", "Cleveland", "Dallas", "Philadelphia", "Columbus",
           "Miami", "Portland", "Los Angeles", "Baltimore", "Charleston", "New York", "Albuquerque", "St. Louis",
-          "Denver", "Virginia Beach", "Atlanta", "Boston", "Sacramento", "Phoenix", "Charlotte", "Minneapolis", "San Diego", "Salt Lake City"]
+          "Denver", "Virginia Beach", "Atlanta", "Boston", "Sacramento", "Phoenix", "Charlotte", "Minneapolis",
+          "San Diego", "Salt Lake City"]
 
 xlOrders = pd.ExcelFile(r'./resources/output/orderIn60Miles.xlsx')
 xlPolys = pd.ExcelFile(r'./resources/output/polygons.xlsx')
@@ -40,15 +79,37 @@ for city in cities:
 
     count = 0
     total = 0
+    pointsInRadius = []
     for oIndex in range(0, len(disOrders)):
         if disOrders[oIndex] > 60:
             break
         point = Point(latOrders[oIndex], lngOrders[oIndex])
         if polygon.contains(point):
+            pointsInRadius.append(point)
             count += 1
             countUS += 1
         total += 1
         totalUS += 1
+
+    latsInRadius = [p.x for p in pointsInRadius]
+    lngsInRadius = [p.y for p in pointsInRadius]
+
+    inRadiusData = {
+        'Lat': latsInRadius,
+        'Lng': lngsInRadius
+    }
+    inRadiusDf = pd.DataFrame(inRadiusData)
+    inRadiusDf.to_excel(writer, sheet_name=city + " Inside Orders")
+
+    alphaShape = alphashape.alphashape(pointsInRadius, 5.0)
+    alphaShapeLat, alphaShapeLng = alphaShape.exterior.coords.xy
+    alphaShapeData = {
+        'Lat': alphaShapeLat,
+        'Lng': alphaShapeLng
+    }
+    alphaShapeDf = pd.DataFrame(alphaShapeData)
+    alphaShapeDf.to_excel(writer, sheet_name=city + " Boundary Orders")
+
     percent = "{0:.0%}".format(count / total)
     percentArray.append(percent)
     print(percent)
@@ -60,5 +121,6 @@ print("US Total:" + percent)
 
 dataPercent = {'City': cities, 'Percentage': percentArray}
 dfPercent = pd.DataFrame(dataPercent)
-dfPercent.to_excel(writer)
+dfPercent.to_excel(writer, sheet_name="Percentage")
 writer.save()
+
